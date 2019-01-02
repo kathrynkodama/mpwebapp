6896ea664772452e19f78635f20bab93d17b3ca3
Kathryn Kodama <kathryn.kodama@ibm.com>
2019-01-02 16:32:36 -0500

    first commit

---
+++ src/main/java/application/rest/JaxrsResource.java
@@ -0,0 +1,28 @@
+package application.rest;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("/jaxrs")
+public class JaxrsResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject getProperties(){
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+        System.getProperties()
+          .entrySet()
+          .stream()
+          .forEach(entry -> builder.add((String)entry.getKey(),
+                                        (String)entry.getValue()));
+        return builder.build();
+
+    }
+
+}
